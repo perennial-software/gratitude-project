061c3649572e0ecef77168c8417dbe3115bbd18f
abetlen <abetlen@gmail.com>
2019-07-16 19:17:48 -0400

    Fix video resizing and inconsistent spacing

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,40 +8,48 @@
     </div>
     <div class="flex-col px-8 mx-auto justify-center" style="max-width: 960px;">
       <div class="text-center mb-8">
-        <h2 class="text-lg font-bold text-gray-100 mb-0">
+        <h2 class="text-lg font-bold text-gray-100">
           Thank you, {{ recipientName }}!
         </h2>
-        <h3 class="text-2xl mb-10 leading-tight text-gray-200">
+        <h3 class="text-2xl leading-tight text-gray-200">
           You had a meaningful impact.
           <br />
           {{ beneficiaryName }} wants to tell you about it.
         </h3>
       </div>
 
-      <div class="flex justify-center mb-8">
+      <div
+        class="flex relative justify-center mb-8"
+        style="padding-top: 56.25%;"
+      >
         <iframe
+          class="absolute inset-0 w-full h-full"
           :src="videoLink"
-          width="640"
-          height="360"
           frameborder="0"
           allow="autoplay; fullscreen"
           allowfullscreen
         ></iframe>
       </div>
 
-      <div class="text-enter mb-8">
+      <div class="text-enter mb-8 text-center">
         It makes a big difference to have you as part of our community. Want to
-        have more impact? Here
-        <span v-if="callsToAction.length === 1">is one opportunity:</span>
-        <span v-if="callsToAction.length === 2">are two opportunities:</span>
-        <span v-if="callsToAction.length === 3">are three opportunities:</span>
+        have more impact?
+        <span v-if="callsToAction.length === 1">
+          Here is one opportunity:
+        </span>
+        <span v-if="callsToAction.length === 2"
+          >Here are two opportunities:
+        </span>
+        <span v-if="callsToAction.length === 3"
+          >Here are three opportunities:
+        </span>
       </div>
 
       <div class="flex justify-center">
         <div
           v-for="(cta, index) in callsToAction"
           :key="index"
-          class="flex-1 text-center mb-8"
+          class="flex-1 text-center mb-8 px-2"
         >
           <button
             class="py-8 px-16 mb-4 bg-gray-100 hover:bg-gray-200 text-xl text-gray-800 shadow-lg rounded-full"
