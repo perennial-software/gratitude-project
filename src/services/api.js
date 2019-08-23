2b9e7fd5217ba7ac655b1753a5f851f7404a0d8a
abetlen <abetlen@gmail.com>
2019-08-23 01:09:35 -0400

    Update api to use cloud function.

--- src/services/api.js
+++ src/services/api.js
@@ -11,46 +11,18 @@ export default {
     });
   },
   postGratitudeMessage(gratitudeMessage) {
-    return new Promise((resolve, reject) => {
-      resolve({
-        id: "1"
-      });
-    });
+    return axios
+      .post(
+        "https://us-central1-fast-synapse-246815.cloudfunctions.net/gratitudeMessage",
+        gratitudeMessage
+      )
+      .then(response => ({ id: response.body.id }));
   },
   getGratitudeMessage(messageId) {
-    return new Promise((resolve, reject) => {
-      resolve({
-        id: messageId,
-        recipientName: "Margie",
-        organizationName: "MCC Toronto",
-        beneficiaryName: "Josie",
-        videoUrl: "https://player.vimeo.com/video/343099753",
-        message:
-          "It makes a big difference to have you as part of our community. Want to have more impact?",
-        callsToAction: [
-          {
-            buttonText: "Volunteer",
-            description:
-              "There are so many other ways to belong to our community.",
-            type: "LINK",
-            link: "https://google.ca"
-          },
-          {
-            buttonText: "Advocate",
-            description:
-              "Advocate for issues concerning MCC Toronto, including LGBTQ and refugee rights.",
-            type: "LINK",
-            link: "https://google.ca"
-          },
-          {
-            buttonText: "Donate",
-            description:
-              "Advocate for issues concerning MCC Toronto, including LGBTQ and refugee rights.",
-            type: "LINK",
-            link: "https://google.ca"
-          }
-        ]
-      });
-    });
+    return axios
+      .get(
+        `https://us-central1-fast-synapse-246815.cloudfunctions.net/gratitudeMessage?messageId=${messageId}`
+      )
+      .then(response => response.body);
   }
 };
