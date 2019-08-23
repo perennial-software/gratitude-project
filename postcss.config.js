5d3458a951efbb3d922b832d288e80b6c1d7d119
abetlen <abetlen@gmail.com>
2019-08-23 02:02:41 -0400

    Disable purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -15,7 +15,7 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer"),
-    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    require("autoprefixer")
+    // ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
