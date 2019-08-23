35cc0bbcdcd3905d24694fcdc06fae3b861a549a
abetlen <abetlen@gmail.com>
2019-08-23 12:46:41 -0400

    Fix purgecss extractor.

--- postcss.config.js
+++ postcss.config.js
@@ -9,13 +9,13 @@ const purgecss = require("@fullhuman/postcss-purgecss")({
   ],
 
   // Include any special characters you're using in this regular expression
-  defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
+  defaultExtractor: content => content.match(/[\w-/:]+(?<!:)/g) || []
 });
 
 module.exports = {
   plugins: [
     require("tailwindcss"),
-    require("autoprefixer")
-    // ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
+    require("autoprefixer"),
+    ...(process.env.NODE_ENV === "production" ? [purgecss] : [])
   ]
 };
