ec4f077781c27af334e1f1c6560edfde00f69b58
abetlen <abetlen@gmail.com>
2019-08-27 09:13:23 -0400

    Added fake gtag service.

--- src/plugins/gtag.js
+++ src/plugins/gtag.js
@@ -1,5 +1,17 @@
+function fake_gtag() {
+  let output = "GTAG: ";
+  for (var i = 0; i < arguments.length; i++) {
+    output += `${arguments[i]}`;
+    if (i + 1 !== arguments.length) {
+      output += ", ";
+    }
+  }
+  console.log(output);
+}
+
 export default {
   install(Vue) {
-    Vue.prototype.$gtag = window.gtag;
+    Vue.prototype.$gtag =
+      process.env.NODE_ENV === "development" ? fake_gtag : window.gtag;
   }
 };
