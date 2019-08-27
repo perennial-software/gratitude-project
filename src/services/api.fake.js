71eaa62b366b959ef4e4ef479384fac4323e1189
abetlen <abetlen@gmail.com>
2019-08-27 09:13:53 -0400

    Updated api fake to persist data.

--- src/services/api.fake.js
+++ src/services/api.fake.js
@@ -1,3 +1,41 @@
+function makeid(length) {
+  var result = "";
+  var characters =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  var charactersLength = characters.length;
+  for (var i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+  return result;
+}
+
+const API_FAKE_DB = "API_FAKE_DB";
+const ID_LENGTH = 16;
+
+function getDB() {
+  return JSON.parse(sessionStorage.getItem(API_FAKE_DB)) || {};
+}
+
+function setDB(db) {
+  sessionStorage.setItem(API_FAKE_DB, JSON.stringify(db));
+}
+
+function insertMessage(id, message) {
+  const db = getDB();
+  const row = {
+    id,
+    ...message
+  };
+  db[id] = row;
+  setDB(db);
+  return row;
+}
+
+function getMessage(id) {
+  const db = getDB();
+  return db[id];
+}
+
 export default {
   getVimeoCredentials() {
     return new Promise(resolve => {
@@ -10,46 +48,14 @@ export default {
   },
   postGratitudeMessage(gratitudeMessage) {
     return new Promise(resolve => {
-      resolve({
-        id: "123456",
-        ...gratitudeMessage
-      });
+      const id = makeid(16);
+      const message = insertMessage(id, gratitudeMessage);
+      resolve(message);
     });
   },
   getGratitudeMessage(messageId) {
     return new Promise(resolve => {
-      resolve({
-        id: messageId,
-        recipientName: "Margie",
-        organizationName: "MCC Toronto",
-        beneficiaryName: "Josie",
-        videoUrl: "https://player.vimeo.com/video/343099753",
-        message:
-          "It makes a big difference to have you as part of our community. Want to have more impact?",
-        callsToAction: [
-          {
-            buttonText: "Volunteer",
-            description:
-              "There are so many other ways to belong to our community.",
-            type: "LINK",
-            link: "https://google.ca"
-          },
-          {
-            buttonText: "Advocate",
-            description:
-              "Advocate for issues concerning MCC Toronto, including LGBTQ and refugee rights.",
-            type: "LINK",
-            link: "https://google.ca"
-          },
-          {
-            buttonText: "Donate",
-            description:
-              "Advocate for issues concerning MCC Toronto, including LGBTQ and refugee rights.",
-            type: "LINK",
-            link: "https://google.ca"
-          }
-        ]
-      });
+      resolve(getMessage(messageId));
     });
   }
 };
