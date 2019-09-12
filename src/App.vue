a7958c9ed47dce8feae84dd1ca6d25b5c64cecf5
abetlen <abetlen@gmail.com>
2019-09-12 15:26:16 -0400

    Fix scrolling ui bug.

--- src/App.vue
+++ src/App.vue
@@ -5,17 +5,18 @@
 </template>
 <style>
 body {
+  min-height: 100vh;
+  display: flex;
+  flex-direction: column;
+}
+#app {
+  display: flex;
+  flex-direction: column;
+  flex-grow: 1;
   font-family: "Rubik", sans-serif;
-  /* background: #34a7ff; */
-  /*
-  background: 
-    linear-gradient(30deg, rgba(0, 0, 0, 0) 58%, rgba(0, 0, 0, .04)  60%, rgba(0, 0, 0, 0) 60%),  
-    linear-gradient(30deg, #34a7ff 60%, #45cafd 60%);
-  */
   background: linear-gradient(to bottom right, #34a7ff 55%, #45cafd);
   background-attachment: fixed;
   background-repeat: no-repeat;
-  min-height: 100vh;
 }
 button {
   outline: none !important;
