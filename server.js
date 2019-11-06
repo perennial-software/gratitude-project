dc184c4920130e7a132a26d036b84a26ed19bd30
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-05 20:04:15 -0500

    Connection to MongoDB works

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+const config = require("config");
 const app = express();
 
 //bodyParser middleware
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
+
+//connect to db
+const db = config.get("mongoURI");
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch(err => console.log(err));
 
 const PORT = process.env.PORT || 5000;
 
