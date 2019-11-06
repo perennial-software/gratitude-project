5f7dc769c6051f006921556cf335d8690f4c857c
Sonia Zaldana Calles <zaldanasonia@gmail.com>
2019-11-06 17:40:39 -0400

    changed variable names so they match what will come in API endpoint

--- client/src/views/Search.vue
+++ client/src/views/Search.vue
@@ -5,25 +5,25 @@
     <div class="container w-2/3 mx-auto">
         <table-component
           :data="[
-          { organization: 'Liftovers', beneficiary: 'Sonia', timestamp: '04/10/1940'},
-          { organization: 'Feed the Kids', beneficiary: 'Ignas', timestamp: '18/06/1942'},
-          { organization: 'Gratitude', beneficiary: 'Alex', timestamp: '25/02/1943'},
-          { organization: 'Chalmers Card', beneficiary: 'Abdullah', timestamp: '07/07/1940'},
+          { recipientName: 'Liftovers', beneficiaryName: 'Sonia', timestamp: '04/10/1940'},
+          { recipientName: 'Feed the Kids', beneficiaryName: 'Ignas', timestamp: '18/06/1942'},
+          { recipientName: 'Gratitude', beneficiaryName: 'Alex', timestamp: '25/02/1943'},
+          { recipientName: 'Chalmers Card', beneficiaryName: 'Abdullah', timestamp: '07/07/1940'},
      ]"
           :show-caption="false"
           :sort-by="organization"
           :sort-order="asc"
           filter-no-results="There are no matching results"
-          filter-placeholder="Search Non-Profit, Beneficiary name or timestamp"
+          filter-placeholder="Search Beneficiary Name, Recipient Name or timestamp"
           filter-input-class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 focus:outline-none focus:shadow-outline"
           table-class="w-full rounded-lg overflow-hidden sm:shadow-lg my-5"
         >
-          <table-column show="organization" label="Non-Profit" header-class="py-4 px-6 bg-grey-lightest font-bold border-b border-grey-light" cell-class="py-4 px-6 bg-grey-lightest border-b border-grey-light"/>
-          <table-column show="beneficiary" label="Beneficiary" header-class="py-4 px-6 bg-grey-lightest font-bold border-b border-grey-light" cell-class="py-4 px-6 bg-grey-lightest border-b border-grey-light"/>
+          <table-column show="recipientName" label="Recipient" header-class="py-4 px-6 bg-grey-lightest font-bold border-b border-grey-light" cell-class="py-4 px-6 bg-grey-lightest border-b border-grey-light"/>
+          <table-column show="beneficiaryName" label="Beneficiary" header-class="py-4 px-6 bg-grey-lightest font-bold border-b border-grey-light" cell-class="py-4 px-6 bg-grey-lightest border-b border-grey-light"/>
           <table-column show="timestamp" label="Timestamp" data-type="date:DD/MM/YYYY" header-class="py-4 px-6 bg-grey-lightest font-bold border-b border-grey-light" cell-class="py-4 px-6 bg-grey-lightest border-b border-grey-light"/>
           <table-column label="Gratitude Message" :sortable="false" :filterable="false" header-class="py-4 px-6 bg-grey-lightest font-bold border-b border-grey-light" cell-class="py-4 px-6 bg-grey-lightest border-b border-grey-light">
             <template slot-scope="row">
-              <a :href="`#${row.Timestamp}`">Link</a>
+              <a :href="`#${row.organization}`">Link</a>
             </template>
           </table-column>
         </table-component>
