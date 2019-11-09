76dd94d948442a8e8849a3046ed864d6bdd450d3
Abdullah Sumsum <abdullahsumsum@Abdullahs-MacBook-Pro.local>
2019-11-09 16:16:49 -0500

    Update Message model

--- models/Message.js
+++ models/Message.js
@@ -3,30 +3,47 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const MessageSchema = new Schema({
-    user: {
-        type: Schema.Types.ObjectId,
-        ref: "users"
-    },
-    beneficiaryName: {
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "users"
+  },
+  beneficiaryName: {
+    type: String,
+    required: true
+  },
+  callsToAction: [
+    {
+      buttonText: {
         type: String,
         required: true
-    },
-    callsToAction: {
-        type: Array,
-        required: true
-    },
-    recipientName: {
+      },
+
+      description: {
         type: String,
         required: true
-    },
-    recipientEmail: {
+      },
+      link: {
         type: String,
         required: true
-    },
-    videoURL: {
-        type: String,
-        required: true
-    },
+      }
+    }
+  ],
+  recipientName: {
+    type: String,
+    required: true
+  },
+  recipientEmail: {
+    type: String,
+    required: true
+  },
+  videoURL: {
+    type: String,
+    required: true
+  },
+  date: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = Message = mongoose.model("messages", MessageSchema);
