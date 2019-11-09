f79683a172eec2b6c3f9f6c91465b30d2a69cb30
Sonia Zaldana Calles <zaldanasonia@gmail.com>
2019-11-09 14:40:50 -0500

    merge with master

--- client/src/router.js
+++ client/src/router.js
@@ -3,12 +3,9 @@ import Router from "vue-router";
 import Home from "./views/Home.vue";
 import Item from "./views/Item.vue";
 import About from "./views/About.vue";
-<<<<<<< HEAD
 import Search from "./views/Search.vue";
-=======
 import Login from "./views/Login.vue";
 
->>>>>>> 71bfc4be876f82f402035b9e972b87433251ec17
 Vue.use(Router);
 
 export default new Router({
@@ -37,15 +34,14 @@ export default new Router({
       component: About
     },
     {
-<<<<<<< HEAD
       path: "/search",
       name: "Search",
       component: Search
-=======
+    },
+    {
       path: "/login",
       name: "Login",
       component: Login
->>>>>>> 71bfc4be876f82f402035b9e972b87433251ec17
     }
   ]
 });
