08bb35f0c0b6339950b8ab3e0bb4376294432dc1
ignaspa <ignaspan@gmail.com>
2019-11-11 13:17:36 -0500

    axios call working, but is there a promise?

--- client/src/services/MessageService.js
+++ client/src/services/MessageService.js
@@ -1,40 +1,30 @@
-// import axios from "axios";
-
-// const apiClient = axios.create({
-//   baseURL: `http://localhost:3000/api` // update later
-//   // can add credentials and such here
-// });
-// export default {
-//   getMessages() {
-//     // axios.defaults.headers.common['Authorization'] = res.data.token;
-//     let lst_messages = [];
-//     // axios.get(baseURL + "/messages")
-//     // .then((response) => {
-//     //   for(let i = 0; i < response.data["messages"].length; i++){
-//     //     lst_messages.push(response.data["messages"][i]);
-//     //   }
-//     // },
-//     // (error) => {
-//     //   console.log("Error getting messages: " + error);
-//     // });
-//     return lst_messages;
-//   },
-//   getMessage(messageID) {
-//     return apiClient.get(`/messages/${messageID}`);
-//   },
-//   postMessage(message) {
-//     return apiClient.post(`/messages`);
-//   }
-// };
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:3000/api` // update later
+  baseURL: 'http://localhost:5000/api' // update later
   // can add credentials and such here
 });
 export default {
   getMessages() {
-    return apiClient.get(`/messages`);
+    console.log("axios call started")
+    axios.defaults.headers.common['Authorization'] = localStorage.token;
+    let lst_messages = [];
+    axios.get("http://localhost:5000/api/messages")
+    .then((response) => {
+      lst_messages = response.data.map(function(item){ 
+        return item; 
+      }) 
+      console.log("answers")
+      console.log(lst_messages)
+      console.log(response.data)
+      console.log("end answers")
+    },
+    (error) => {
+      console.log("Error getting messages: " + error);
+    });
+    console.log("seeing this before i have answers...?")
+    console.log(lst_messages)
+    return lst_messages;
   },
   getMessage(messageID) {
     return apiClient.get(`/messages/${messageID}`);
---
+++ client/src/views/Userdashboard.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <nav-bar />
+    <div
+      class="flex-col px-4 md:px-8 mx-auto justify-center"
+      style="max-width: 960px;"
+    >
+      <div class="mb-8 text-left text-gray-100">
+        <h1 class="text-lg font-bold mb-8">Welcome {{user}}</h1>
+
+        <button class="btn btn-blue">
+            <router-link v-else :to="{ name: 'Search' }" class="navItem text-xl">Search</router-link>
+        </button>
+        <button class="btn btn-blue">
+            <router-link v-else :to="{ name: 'Create' }" class="navItem text-xl">Create</router-link>
+        </button>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import NavBar from "@/components/NavBar.vue";
+import VueRouter from 'vue-router';
+Vue.use(VueRouter);
+
+export default {
+  components: {
+    NavBar
+  }
+  data: {
+      user = localStorage.username
+  }
+};
+</script>
+
+<style>
+  .btn {
+    @apply font-bold py-2 px-4 rounded;
+  }
+  .btn-blue {
+    @apply bg-blue-500 text-white;
+  }
+  .btn-blue:hover {
+    @apply bg-blue-700;
+  }
+</style>
